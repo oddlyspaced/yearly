a8343b7c8224c60c53c1b6746a8a5e76c43d1ed2
oddlyspaced <oddlyspaced1@gmail.com>
2026-01-01 12:29:38 +0530

    chore: calendar list screen nav setup

--- src/core/navigation/NavigationRouter.tsx
+++ src/core/navigation/NavigationRouter.tsx
@@ -1,6 +1,7 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { TNavigationRouterProps } from './NavigationRouterProps';
 import { HomeScreen } from '../../features/home/screens/HomeScreen';
+import { CalendarListScreen } from '../../features/calendar/screens/CalendarListScreen';
 
 const AppNavigator = createNativeStackNavigator<TNavigationRouterProps>();
 
@@ -13,6 +14,10 @@ export const NavigationRouter = () => {
 			}}
 		>
 			<AppNavigator.Screen component={HomeScreen} name='HomeScreen' />
+			<AppNavigator.Screen
+				component={CalendarListScreen}
+				name='CalendarListScreen'
+			/>
 		</AppNavigator.Navigator>
 	);
 };
--- src/core/navigation/NavigationRouterProps.tsx
+++ src/core/navigation/NavigationRouterProps.tsx
@@ -1,3 +1,4 @@
 export type TNavigationRouterProps = {
 	HomeScreen: {};
+	CalendarListScreen: {};
 };
