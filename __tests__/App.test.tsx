13ce7305775cf420dbad74a5e53cb3001b507872
oddlyspaced <oddlyspaced1@gmail.com>
2026-03-05 00:01:38 +0530

    chore: re init

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -4,10 +4,10 @@
 
 import React from 'react';
 import ReactTestRenderer from 'react-test-renderer';
-import App from '../src/App';
+import App from '../App';
 
 test('renders correctly', async () => {
-	await ReactTestRenderer.act(() => {
-		ReactTestRenderer.create(<App />);
-	});
+  await ReactTestRenderer.act(() => {
+    ReactTestRenderer.create(<App />);
+  });
 });
